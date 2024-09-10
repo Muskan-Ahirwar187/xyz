1e555f1bad676141422c36582d73a50119b6164a
Muskan-Ahirwar187 <muskanahirwar187@gmail.com>
2024-09-10 18:41:09 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,12 @@
+
+ /*JavaScript: Adding Interactivity
+Create a file named script.js. For now, let’s keep it simple:
+
+javascript
+Copy code
+// script.js
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio is ready!');
+});
